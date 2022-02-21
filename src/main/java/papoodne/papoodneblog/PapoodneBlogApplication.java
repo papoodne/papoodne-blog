4179464f218beba26b409fb9318071850107f9d1
parkSeongJu <parksj914@argonet.co.kr>
2022-02-21 23:35:25 +0900

    initial commit

---
+++ src/main/java/papoodne/papoodneblog/PapoodneBlogApplication.java
@@ -0,0 +1,13 @@
+package papoodne.papoodneblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapoodneBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapoodneBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/papoodne/papoodneblog/PapoodneBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package papoodne.papoodneblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapoodneBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
